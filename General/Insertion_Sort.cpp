a542ca7e2996e3bd90b50a69dd47af8000f7ba60
Vedant Jain <vedant.jain.indore@gmail.com>
2023-02-24 23:20:33 +0530

    edited

--- General/Insertion_Sort.cpp
+++ General/Insertion_Sort.cpp
@@ -1,37 +1,39 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
-int main(){
-    vector<int>arr{10,1,7,6,14,9};
+void printArr(vector<int>arr){
+    for(int i = 0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}
+void insertionSort(vector<int>&arr){
     int n = arr.size();
-
-    for(int round = 1; round < n; round++){
-        // Step A: Fetch
+    for(int round = 1;round<n;round++){ // number of rounds is n-1 for n elements
+        // Step - 1 Pick the element
         int val = arr[round];
-        int j = round-1;
-        // Step B: Compare
-        for(; j>0; j--){
+        int j = round - 1; // So that we can access outside loop
+        for(;j>=0;j--){ // compare with all the elements behind the ith element
             if(arr[j] > val){
-                // Step C: Shift
+                // Do shifting operation
                 arr[j+1] = arr[j];
             }
             else{
-                // Rukna hai
-                break;
+                break; // simply exit the loop 
             }
         }
-        // Step D: Copy
+        // Copying step
         arr[j+1] = val;
     }
-
-    // Printing
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << endl;
-    }
 }
-
+int main(){
+    vector<int>arr{10,1,4,8,5,7};
+    cout<<"Printing before sorted "<<endl;
+    printArr(arr);
+    cout<<"Printing array after sorting "<<endl;
+    insertionSort(arr);
+    printArr(arr);
+    return 0;
+}
 
 // TC: O(n^2) in normal case, O(n) in best case, O(n^2) in worst case
 // SC: O(1)
