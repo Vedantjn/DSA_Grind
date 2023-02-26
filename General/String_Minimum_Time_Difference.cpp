f2a972f5244dee3232f80f3ad00cb3fb4cf3ae8f
Vedant Jain <vedant.jain.indore@gmail.com>
2023-02-26 15:33:20 +0530

    completed

--- General/String_Minimum_Time_Difference.cpp
+++ General/String_Minimum_Time_Difference.cpp
@@ -5,39 +5,40 @@
 #include<limits.h>
 using namespace std;
 
-int findMinDifference(vector<string>&timePoints){
-    // Step 1: Convert time string into minutes integer value
-    vector<int>minutes;
+    int findMinDifference(vector<string>& timePoints) {
+        //step1: convert time string into minutes integer value
+        vector<int> minutes;
 
-    for(int i = 0; i < timePoints.size(); i++){
-        string curr = timePoints[i];
-        int hours = stoi(curr.substr(8, 2) );
-        int min = stoi(curr.substr(3, 2));
+        for(int i=0; i<timePoints.size(); i++) {
+            string curr = timePoints[i];
 
-        int totalMinutes = hours*60 + min;
-        minutes.push_back(totalMinutes);
+            int hours = stoi( curr.substr(0,2) );
+            int min = stoi( curr.substr(3,2) );
+            int totalMinutes = hours*60 + min;
+            minutes.push_back(totalMinutes);
+        }
+
+        //Step2: sort
+        sort(minutes.begin(), minutes.end());
+
+        //Step3: difference and calculate min diff
+        int mini = INT_MAX;
+        int n = minutes.size();
+
+        for(int i=0; i<n-1; i++) {
+            int diff = minutes[i+1] - minutes[i];
+            mini = min(mini, diff);
+        }
+
+        //something missing - THIS IS THE GAME
+        int lastDiff1 = (minutes[0] + 1440) - minutes[n-1];
+        int lastDiff2 = minutes[n-1] - minutes[0];
+        int lastDiff = min(lastDiff1, lastDiff2);
+        mini = min(mini, lastDiff);
+
+        return mini;
+
     }
-
-    // Step 2: Sort
-    sort(minutes.begin(), minutes.end());
-
-    // Step 2: Difference nad calculate difference
-    int mini = INT_MAX;
-    int n = minutes.size();
-
-    for(int i = 0; i < n-1; i++){
-        int difference = minutes[i+1] - minutes[i];
-        mini = min(mini, difference);
-    }
-
-    // THIS IS THE GAME
-    int lastDiff1 = (minutes[0] + 1440) - minutes[n-1];
-    int lastDiff2 = minutes[n-1] - minutes[0];
-    int lastDiff = min(lastDiff1, lastDiff2);
-    mini = min(mini, lastDiff);
-
-    return mini;
-}
 
 int main(){
     vector<string>timePoints{"23:59", "00:00"};
--- General/Strings_Remove_Duplicated.cpp
+++ General/Strings_Remove_Duplicated.cpp
@@ -3,28 +3,22 @@
 #include<vector>
 using namespace std;
 
-string removeDuplicates(string s){
+    string removeDuplicates(string s) {
+        string ans ="";
+        int i = 0;
 
-    string ans = "";
-    int i = 0;
+        while( i < s.length() ) {
 
-    while(i < s.length()){
-
-        if(ans.length() > 0){
-            if(ans[ans.length() - 1] == s[i]){
-                 ans.pop_back();
-            }
-            else{
-                ans.push_back(s[i]);
-            }
+                if( ans.length() > 0  &&  ans[ans.length() - 1] == s[i]) {
+                    ans.pop_back();
+                }
+                else {
+                    ans.push_back(s[i]);
+                }
+            i++;
         }
-        else{
-            ans.push_back(s[i]);
-        }
-        i++;
-    }
         return ans;
-}
+    }
 
 
 int main(){
